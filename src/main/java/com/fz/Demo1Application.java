d0fd3dd644accf9951c19e2fd783b34ab5b9cbbe
furkan <furkanzayimoglu@gmail.com>
2016-09-28 15:09:02 +0300

    Add files via upload

---
+++ src/main/java/com/fz/Demo1Application.java
@@ -0,0 +1,23 @@
+package com.fz;
+
+import java.util.Arrays;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class Demo1Application {
+
+	public static void main(String[] args) {
+        ApplicationContext ctx = SpringApplication.run(Demo1Application.class, args);
+
+        System.out.println("Spring-Boot Bean Listesi");
+
+        String[] beanNames = ctx.getBeanDefinitionNames();
+        Arrays.sort(beanNames);
+        for (String beanName : beanNames) {
+            System.out.println(beanName);
+        }
+	}
+}
